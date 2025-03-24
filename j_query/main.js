61c87733d1c30e2e677d936f17d8bdfb85f142ed
janusom <janusom@vcu.edu>
2025-03-24 09:59:43 -0400

    added bunch of stuff (Exercices plus portfolio)

---
+++ j_query/main.js
@@ -0,0 +1,20 @@
+$(".dark-btn").click(
+    function(){
+    $(".square").toggleClass("dark");
+    $("body").toggleClass("dark");
+  });
+
+  $(".spin-btn").click(
+    function(){
+    $(".square").toggleClass("spin")
+  });
+
+  $(".reveal-btn").click(
+    function(){
+    $(".lion").addClass("reveal")
+    $(".reveal-btn").hide();
+  });
+
+  $(function(){
+    $(".draggable").draggable()
+  });
